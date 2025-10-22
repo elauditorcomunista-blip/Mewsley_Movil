76538696cd83695dac8beb3621914e3c0edf6ee4
elauditorcomunista-blip <el.auditor.comunista@gmail.com>
2025-10-22 11:39:57 -0600

    Update bienvenida.js

--- pluggins/bienvenida.js
+++ pluggins/bienvenida.js
@@ -1,16 +1,17 @@
-// 📂 plugins/bienvenida.js
-const handler = async (m, { conn }) => {};
-
-// Escucha eventos del grupo
-handler.before = async (m, { conn, participants }) => {
-  if (!m.isGroup) return;
-
-  for (const user of m.action === 'add' ? m.participants : []) {
-    if (m.action === 'add') {
-      const texto = `👋 ¡Bienvenido/a @${user.split('@')[0]} al grupo *${m.chatName || ''}*! 🌟\nDisfruta del grupo y respeta las reglas.`
-      conn.sendMessage(m.chat, { text: texto, mentions: [user] });
+const bienvenida = async (XeonBotInc) => {
+  XeonBotInc.ev.on('group-participants.update', async (update) => {
+    try {
+      const { id, participants, action } = update; // id = grupo, participants = array de usuarios, action = 'add', 'remove', etc.
+      if (action === 'add') {
+        for (const user of participants) {
+          const texto = `👋 ¡Bienvenido/a @${user.split('@')[0]} al grupo! 🌟\nDisfruta del grupo y respeta las reglas.`;
+          await XeonBotInc.sendMessage(id, { text: texto, mentions: [user] });
+        }
+      }
+    } catch (err) {
+      console.error('Error en bienvenida:', err);
     }
-  }
+  });
 };
 
-export default handler;
+module.exports = bienvenida;
